286c52ae189e27aa02b31abeb6791452e7bf353a
Nathan Lowry <nathandl@gmail.com>
2019-03-21 16:12:34 -0500

    New aliases and install for dev-pi

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -2,3 +2,7 @@ alias pmr='python manage.py runserver'
 alias pmshell='python manage.py shell'
 alias pmdb='python manage.py dbshell'
 alias pycache_clear='find . | grep -E “(__pycache__|\.pyc|\.pyo$)” | xargs rm -rf'
+
+# dev-pi: https://devpi.net/docs/devpi/devpi/stable/%2Bd/index.html
+alias start_devpi='devpi-server --start'
+alias stop_devpi='devpi-server --stop'
--- python/install.sh
+++ python/install.sh
@@ -7,8 +7,8 @@ git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git ~/.pyenv/plugins/p
 echo -e "\n>> Installing Python 2 and 3"
 pyenv install 2.7.14
 pyenv install 3.6.4
-pyenv install 3.6.5
-pyenv global 3.6.4
+pyenv install 3.7.0
+pyenv global 3.7.0
 
 # Install pipenv
-pip install pipenv jupyter
+pip install pipenv jupyter devpi-server devpi-web

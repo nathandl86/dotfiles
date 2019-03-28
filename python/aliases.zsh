f5b08672635d2496dd1d3b74ac668896e23b0c37
Nathan Lowry <nathandl@gmail.com>
2019-03-27 22:52:20 -0500

    Updated based on the new computer being built out.

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -4,5 +4,6 @@ alias pmdb='python manage.py dbshell'
 alias pycache_clear='find . | grep -E “(__pycache__|\.pyc|\.pyo$)” | xargs rm -rf'
 
 # dev-pi: https://devpi.net/docs/devpi/devpi/stable/%2Bd/index.html
+# devpi-server --start --init will be need the first time
 alias start_devpi='devpi-server --start'
 alias stop_devpi='devpi-server --stop'

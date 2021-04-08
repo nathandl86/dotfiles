df9b9c671ab4570436199b9279f65ddb5a7a6b99
Nathan Lowry <nathandl@gmail.com>
2021-04-08 08:13:48 -0400

    Adds mac network helper alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -54,3 +54,8 @@ alias ve='mvim -v ~/.vimrc'
 alias ze='mvim -v ~/.zshrc'
 
 alias onepassLogin="/usr/local/bin/oplogin && source /tmp/.opsession"
+
+# mac netstat that sort of works
+function macns() {
+  netstat -Watnlv | grep LISTEN | awk '{"ps -o comm= -p " $9 | getline procname;colred="\033[01;31m";colclr="\033[0m"; print cred "proto: " colclr $1 colred " | addr.port: " colclr $4 colred " | pid: " colclr $9 colred " | name: " colclr procname;  }' | column -t -s "|"
+}

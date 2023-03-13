f6e6a47bbf8fc7339dd11658e113c6b16de87c21
Nathan Lowry <nathandl@gmail.com>
2023-03-13 19:27:43 -0400

    config changes

--- python/install.sh
+++ python/install.sh
@@ -5,11 +5,11 @@ git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git ~/.pyenv/plugins/p
 
 # Installing Python 2 and 3
 echo -e "\n>> Installing Python 2 and 3"
-pyenv install 3.7.10
-pyenv install 3.8.12
+pyenv install 3.7.13
 pyenv install 3.9.7
+pyenv isntall 3.10.8
 
-pyenv global 3.9.7
+pyenv global 3.10.8
 
 echo -e '\n>> Installing pypi packages'
 pip install --upgrade pip

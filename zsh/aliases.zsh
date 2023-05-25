5d4ddcafb5168445c019b146622e782d38126296
Nathan Lowry <nathandl@gmail.com>
2023-05-25 16:16:05 -0400

    configuration for dual x86 & arm64 brew

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -58,3 +58,6 @@ alias onepassLogin="/usr/local/bin/oplogin && source /tmp/.opsession"
 function macns() {
   netstat -Watnlv | grep LISTEN | awk '{"ps -o comm= -p " $9 | getline procname;colred="\033[01;31m";colclr="\033[0m"; print cred "proto: " colclr $1 colred " | addr.port: " colclr $4 colred " | pid: " colclr $9 colred " | name: " colclr procname;  }' | column -t -s "|"
 }
+
+# for x86 brew
+alias brew86='arch -x86_64 /usr/local/homebrew/bin/brew'
